4158795430aa0cce30103a2b3c75ace75471c881
Cheniqua Breaux <trenai06@gmail.com>
2024-08-08 17:05:56 -0400

    some file path changes

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+
+const path = require('path');
 const app = express();
 
+app.use(express.static(path.join(__dirname, '/public')))
 app.get('/', (req,res) => {
-    res.send('Hello, world!');
+    res.sendFile(path.join(__dirname, 'public/HTML/index.html'));
 });
 
 app.listen(3000, () => {
